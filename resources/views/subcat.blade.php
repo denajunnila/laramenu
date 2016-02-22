195de609246e707bc16a385b5475b04e1bc129a1
denajunnila <dena.junnila@sbcglobal.net>
2016-02-22 11:30:41 -0800

    filtered empty item pages

--- resources/views/subcat.blade.php
+++ resources/views/subcat.blade.php
@@ -9,10 +9,11 @@
 <!-- link layout to content here -->
 @section('content')
       <!-- display title on page-->
-      <h1> Sub Menu</h1>  
+      
+      <h1> Sub Menu </h1> 
+
       <!-- Go thru the given database  -->
       @foreach($menu as $menus) 
-
                 <!-- Divide page into 3 columns shift into line when page is small-->
                 <div class="col-sm-4">
                     <!-- Display current image from image folder -->
@@ -20,7 +21,13 @@
                     <!-- create link using current id from menu item
                     to the next item page 
                     and display title -->
+                    @if($menus->cat_id!=4 && $menus->cat_id!=5 && $menus->cat_id!=8 
+                        && $menus->cat_id!=9 && $menus->cat_id!=10 && $menus->cat_id!=12)
                     <h1><a href=" /item/{{$menus->id}} ">{{ $menus->title }}</a></h1>
+                    @else
+                    <h1>{{ $menus->title }}</h1>
+                    @endif
+
 
                </div>
 
